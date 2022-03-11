898a301c6652a546343df8537522bc8884a512bd
carlospozu <carpoz774@alu.centroafuera.es>
2022-03-11 09:07:15 +0100

    mochila echa quedan cositas

---
+++ src/main/java/com/example/pokemon/Fondo.java
@@ -0,0 +1,15 @@
+package com.example.pokemon;
+
+import java.io.File;
+
+public class Fondo {
+    File fondo;
+    File fondo1;
+    File fondo2;
+
+    public Fondo(File fondo, File fondo1, File fondo2){
+        this.fondo=fondo;
+        this.fondo1=fondo1;
+        this.fondo2=fondo2;
+    }
+}
